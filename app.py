8b6041df67b47c5001b3490d1030c195dc17775b
TheBrainGamingSingh <itsmanpreetsinghjuneja@gmail.com>
2020-10-18 23:24:47 +0530

    added REST api for the model

--- app.py
+++ app.py
@@ -1,18 +1,56 @@
-import numpy as np
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 import pickle
+import json
+import re
+import numpy as np
 
-from flask import Flask, request, jsonify, render_template
-from flask_restful import Resource, Api
-
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+words = stopwords.words("english")
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('query')
 
-class HelloWorld(Resource):
+MODEL_PATH = './model/RandomForest.pkl'
+
+with open(MODEL_PATH, 'rb') as file:
+    model = pickle.load(file)
+
+with open("./data/mapper.json") as infile:
+    label_mapper = json.load(infile)
+
+def clean_and_stem(text):
+    return [" ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower() for x in [text]]
+
+# Endpoints
+class ComplaintClassifier(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'Welcome!': 'This is a sample response of ComplaintClassifier.'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(ComplaintClassifier, '/')
+
+class PredictClass(Resource):
+    def get(self):
+        args = parser.parse_args()
+        user_query = clean_and_stem(args['query'])
+
+
+        label = model.predict(user_query)[0]
+        probs = model.predict_proba(user_query)[0]
+
+        prediction = label_mapper[str(label)]
+        confidence = round(probs[label], 4) * 100
+        output = {'prediction': prediction, 'confidence': str(confidence) + ' %'}
+
+        return output
+
+api.add_resource(PredictClass, '/predict')
 
 if __name__ == '__main__':
     app.run(debug=True)
